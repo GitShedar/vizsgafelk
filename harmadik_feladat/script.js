0ed724bf4ff3b8c759adeee733bc936e0d1c4519
RekaKR <RekaKR@users.noreply.github.com>
2021-03-23 18:25:03 +0100

    Update

--- harmadik_feladat/script.js
+++ harmadik_feladat/script.js
@@ -1,24 +1,20 @@
-function pageLoad() {
-  //Ide írjuk a feladatot!
+//Ide írjuk a feladatot!
 
-  class Resident {
-    constructor(name, age, hasRegistred = false) {
-      this.name = name
-      this.age = age
-      this.hasRegistred = hasRegistred
-    }
-
+class Resident {
+  constructor(name, age, hasRegistred = false) {
+    this.name = name
+    this.age = age
+    this.hasRegistred = hasRegistred
   }
 
-  class VoterCounter {
-
-  }
-
-
-  /*
-  Implement a canVote instance method on the Resident Class, which returns true only if the instantiated person is older than 17, and has registered.
-  Then implement a static method on the VoterCounter class (getNumberOfVoters) which receives an array of person objects, and returns the count of those who can vote.
-  */
 }
 
-window.addEventListener('load', pageLoad)
+class VoterCounter {
+
+}
+
+
+/*
+Implement a canVote instance method on the Resident Class, which returns true only if the instantiated person is older than 17, and has registered.
+Then implement a static method on the VoterCounter class (getNumberOfVoters) which receives an array of person objects, and returns the count of those who can vote.
+*/
