628aee697834e519fb65f89e845438165428684c
RekaKR <RekaKR@users.noreply.github.com>
2021-03-23 18:23:17 +0100

    Add task 3

---
+++ harmadik_feladat/script.js
@@ -0,0 +1,48 @@
+function pageLoad() {
+  //Ide írjuk a feladatot!
+
+  class Resident {
+    constructor(name, age, hasRegistred = false) {
+      this.name = name
+      this.age = age
+      this.hasRegistred = hasRegistred
+    }
+
+  }
+
+  getNumberOfVoters() {
+
+  }
+
+
+  /*
+  Implement a canVote instance method on the Resident Class, which returns true only if the instantiated person is older than 17, and has registered.
+  Then implement a static method on the VoterCounter class (getNumberOfVoters) which receives an array of person objects, and returns the count of those who can vote.
+  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  class Resident {{
+
+
+
+
+
+    sumOfWithdrawalsByTeengagers(persons);
+  }
+
+  window.addEventListener('load', pageLoad)
