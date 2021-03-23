d5beae0b415e762d75e6e3128b8f5cba4346af00
RekaKR <RekaKR@users.noreply.github.com>
2021-03-23 18:03:20 +0100

    Create repo

---
+++ script.js
@@ -0,0 +1,38 @@
+function pageLoad() {
+  [
+    {
+      "user": { "name": "Kovács Béla", "age": 25 },
+      "type": "withdraw",
+      "amount": 3500,
+      "currency": "HUF"
+    },
+    {
+      "user": { "name": "Kis Pista", "age": 19 },
+      "type": "withdraw",
+      "amount": 200,
+      "currency": "HUF"
+    },
+    {
+      "user": { "name": "Almafa Mariann", "age": 18 },
+      "type": "withdraw",
+      "amount": 3500,
+      "currency": "HUF"
+    },
+    {
+      "user": { "name": "Körtefa Ilona", "age": 26 },
+      "type": "no",
+      "amount": 6000,
+      "currency": "HUF"
+    },
+    {
+      "user": { "name": "Pogácsa Miklós", "age": 18 },
+      "type": "no",
+      "amount": 200,
+      "currency": "HUF"
+    },
+  ]
+
+
+}
+
+window.addEventListener('load', pageLoad)
